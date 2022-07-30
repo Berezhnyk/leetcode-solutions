e1c46dfd051a9cbd7e881ed05d57f57369fc2b60
Ivan Berezhnyk <ivanberezhnyk@gmail.com>
2022-07-30 11:54:52 +0300

    Added more tests top mergeTwoLists problem including benchmark

--- solutions/21_mergeTwoLists/mergeTwoLists_test.go
+++ solutions/21_mergeTwoLists/mergeTwoLists_test.go
@@ -94,3 +94,131 @@ func Test_mergeTwoLists4(t *testing.T) {
 		l3 = l3.Next
 	}
 }
+
+func Test_mergeTwoLists5(t *testing.T) {
+	l1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 4,
+			},
+		},
+	}
+	l2 := &ListNode{
+		Val: 1,
+	}
+	l3 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 4,
+				},
+			},
+		},
+	}
+
+	r := mergeTwoLists(l1, l2)
+	for l3.Next != nil {
+		assert.Equal(t, r.Val, l3.Val)
+		r = r.Next
+		l3 = l3.Next
+	}
+}
+
+func Test_mergeTwoLists6(t *testing.T) {
+	l1 := &ListNode{
+		Val: 1,
+	}
+	l2 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 4,
+			},
+		},
+	}
+
+	l3 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 4,
+				},
+			},
+		},
+	}
+
+	r := mergeTwoLists(l1, l2)
+	for l3.Next != nil {
+		assert.Equal(t, r.Val, l3.Val)
+		r = r.Next
+		l3 = l3.Next
+	}
+}
+
+func Benchmark_mergeTwoLists(b *testing.B) {
+	b.StopTimer()
+	b.ReportAllocs()
+	b.StartTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			mergeTwoLists(&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+					},
+				},
+			}, &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val: 4,
+					},
+				},
+			})
+
+			mergeTwoLists(&ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val: 5,
+							Next: &ListNode{
+								Val: 6,
+							},
+						},
+					},
+				},
+			}, &ListNode{
+				Val: 1,
+				Next: &ListNode{
+					Val: 2,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val: 5,
+							Next: &ListNode{
+								Val: 6,
+							},
+						},
+					},
+				},
+			})
+
+			mergeTwoLists(nil, nil)
+		}
+	})
+}
