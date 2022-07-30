47fddef200785e353203fe61bf7c407b98420d93
Ivan Berezhnyk <ivanberezhnyk@gmail.com>
2022-07-30 11:38:58 +0300

    Solved mergeTwoLists problem

---
+++ solutions/21_mergeTwoLists/mergeTwoLists.go
@@ -0,0 +1,60 @@
+package mergetwolists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	var head *ListNode = nil
+
+	if (*list1).Val <= (*list2).Val {
+		head = &ListNode{
+			Val: (*list1).Val,
+		}
+		list1 = (*list1).Next
+	} else {
+		head = &ListNode{
+			Val: (*list2).Val,
+		}
+		list2 = (*list2).Next
+	}
+
+	current := head
+	for {
+		if list2 == nil && list1 == nil {
+			return head
+		}
+
+		if list1 == nil {
+			(*current).Next = list2
+			return head
+		}
+
+		if list2 == nil {
+			(*current).Next = list1
+			return head
+		}
+
+		if (*list1).Val <= (*list2).Val {
+			(*current).Next = &ListNode{
+				Val: (*list1).Val,
+			}
+			list1 = (*list1).Next
+
+		} else {
+			(*current).Next = &ListNode{
+				Val: (*list2).Val,
+			}
+			list2 = (*list2).Next
+		}
+		current = (*current).Next
+	}
+}
---
+++ solutions/21_mergeTwoLists/mergeTwoLists_test.go
@@ -0,0 +1,79 @@
+package mergetwolists
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_mergeTwoLists1(t *testing.T) {
+	l1 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 4,
+			},
+		},
+	}
+	l2 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 3,
+			Next: &ListNode{
+				Val: 4,
+			},
+		},
+	}
+	l3 := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 1,
+			Next: &ListNode{
+				Val: 2,
+				Next: &ListNode{
+					Val: 3,
+					Next: &ListNode{
+						Val: 4,
+						Next: &ListNode{
+							Val: 4,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	r := mergeTwoLists(l1, l2)
+	for l3.Next != nil {
+		assert.Equal(t, r.Val, l3.Val)
+		r = r.Next
+		l3 = l3.Next
+	}
+}
+
+func Test_mergeTwoLists2(t *testing.T) {
+	var l1 *ListNode = nil
+	var l2 *ListNode = nil
+	var l3 *ListNode = nil
+
+	r := mergeTwoLists(l1, l2)
+	assert.Equal(t, r, l3)
+}
+
+func Test_mergeTwoLists3(t *testing.T) {
+	var l1 *ListNode = nil
+	l2 := &ListNode{
+		Val: 0,
+	}
+	l3 := &ListNode{
+		Val: 0,
+	}
+
+	r := mergeTwoLists(l1, l2)
+	for l3.Next != nil {
+		assert.Equal(t, r.Val, l3.Val)
+		r = r.Next
+		l3 = l3.Next
+	}
+}
