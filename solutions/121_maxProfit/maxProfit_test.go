50ca2eb58da693ca9f1802ac7dd9ba5bfa636032
Ivan Berezhnyk <ivanberezhnyk@gmail.com>
2022-08-01 13:45:24 +0300

    fixed tests

--- solutions/121_maxProfit/maxProfit_test.go
+++ solutions/121_maxProfit/maxProfit_test.go
@@ -10,6 +10,8 @@ func Test_maxProfit(t *testing.T) {
 	assert.Equal(t, 5, maxProfit([]int{7, 1, 5, 3, 6, 4}))
 	assert.Equal(t, 0, maxProfit([]int{7, 6, 4, 3, 1}))
 	assert.Equal(t, 2, maxProfit([]int{2, 4, 1}))
+	assert.Equal(t, 0, maxProfit([]int{}))
+	assert.Equal(t, 0, maxProfit([]int{42}))
 }
 
 func Benchmark_maxProfit(b *testing.B) {
@@ -18,6 +20,7 @@ func Benchmark_maxProfit(b *testing.B) {
 	b.StartTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
+			maxProfit([]int{7, 1, 5, 3, 6, 4, 20, 3, 4, 8, 13, 11, 15, 17})
 			maxProfit([]int{7, 1, 5, 3, 6, 4})
 			maxProfit([]int{7, 6, 4, 3, 1})
 		}
